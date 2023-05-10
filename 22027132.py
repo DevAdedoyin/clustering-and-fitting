e3773a14ed68e6f405766d6663d7769326c041d5
Adedoyin <isradeoluwa@gmail.com>
2023-05-10 15:37:34 +0100

    Cluster code done

--- 22027132.py
+++ 22027132.py
@@ -12,6 +12,12 @@ import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import scale
 from scipy.optimize import curve_fit
+import cluster_tools as ct
+import sklearn.metrics as skmet
+import os
+
+
+os.environ["OMP_NUM_THREADS"] = '1'
 
 # Function to read the dataset
 def read_worldbank_dataset():
@@ -24,8 +30,7 @@ def read_worldbank_dataset():
     worldbank_dataframe = pd.read_csv("worldbank_data.csv", skiprows=4)
         
     # Retrieve relevant indicators
-    indicators = ['Urban population growth (annual %)', 
-                  'Arable land (% of land area)']
+    indicators = ['Urban population growth (annual %)',]
     dataframe = worldbank_dataframe[worldbank_dataframe['Indicator Name']
                                     .isin(indicators)]
     
@@ -42,19 +47,113 @@ def read_worldbank_dataset():
     return dataframe
 
 
-dataframe = read_worldbank_dataset()
-print(dataframe.describe())
+worldbank_dataframe = read_worldbank_dataset()
+print(worldbank_dataframe.describe())
+
+
+dataframe = worldbank_dataframe[["1965", "1970", "1975", "1980", "1985", 
+                                 "1990", "1995", "2010", "2020"]].copy()
+print(dataframe)
+
 
 # Drop rows with NaN in 2020
-cleaned_2020_df = dataframe[dataframe["2020"].notna()]
-print(cleaned_2020_df.describe())
+cleaned_dataframe = dataframe.dropna()
+print(cleaned_dataframe.describe())
 
-cleaned_2020_df_ = cleaned_2020_df[["Indicator Name", "1998", "2000", 
-                                    "2018", "2020"]].copy()
-print(cleaned_2020_df_.describe())
+cleaned_dataframe = cleaned_dataframe.reset_index()
+cleaned_dataframe = cleaned_dataframe.drop("index", axis=1)
 
-#grouped = cleaned_2020_df_.groupby('Indicator Name')
+print(cleaned_dataframe.corr())
+ct.map_corr(cleaned_dataframe, 9)
+pd.plotting.scatter_matrix(cleaned_dataframe, figsize=(9.0, 9.0))
+plt.tight_layout() # helps to avoid overlap of labels
+plt.show()
 
-#print(grouped.describe().T)
+fitted_dataframe = cleaned_dataframe[["1970", "2010"]].copy()
+
+corr = fitted_dataframe.corr()
+print(corr)
+
+# normalise, store minimum and maximum
+normalized_dataframe, dataframe_min, dataframe_max = ct.scaler(
+    fitted_dataframe)
+
+print()
+print("n score")
+# loop over number of clusters
+for ncluster in range(2, 7):
+    # set up the clusterer with the number of expected clusters
+    kmeans = KMeans(n_clusters=ncluster)
+    # Fit the data, results are stored in the kmeans object
+    kmeans.fit(normalized_dataframe) # fit done on x,y pairs
+    labels = kmeans.labels_
+    # extract the estimated cluster centres
+    cen = kmeans.cluster_centers_
+    # calculate the silhoutte score
+    print(ncluster, skmet.silhouette_score(normalized_dataframe, labels))
+
+ncluster = 7
+
+# set up the clusterer with the number of expected clusters
+kmeans = KMeans(n_clusters=ncluster)
+
+# Fit the data, results are stored in the kmeans object
+kmeans.fit(normalized_dataframe) # fit done on x,y pairs
+labels = kmeans.labels_
+
+
+# cluster by cluster
+plt.figure(figsize=(7.0, 7.0))
+cm = plt.cm.get_cmap('tab10')
+plt.scatter(normalized_dataframe["1970"], normalized_dataframe["2010"], 
+            10, labels, marker="o", cmap=cm)
+
+# extract the estimated cluster centres
+cen = kmeans.cluster_centers_
+
+xcen = cen[:, 0]
+ycen = cen[:, 1]
+plt.scatter(xcen, ycen, c="k", marker="d", s=45)
+
+plt.title("Annual Urban population growth(1970 & 2010)")
+plt.xlabel("1970")
+plt.ylabel("2010")
+
+plt.show()
+
+
+###################UNNORMALIZED DATA###########################
+ncluster = 7
+
+# set up the clusterer with the number of expected clusters
+kmeans = KMeans(n_clusters=ncluster)
+
+# Fit the data, results are stored in the kmeans object
+kmeans.fit(fitted_dataframe) # fit done on x,y pairs
+labels = kmeans.labels_
+
+
+# cluster by cluster
+plt.figure(figsize=(7.0, 7.0))
+cm = plt.cm.get_cmap('tab10')
+plt.scatter(fitted_dataframe["1970"], fitted_dataframe["2010"], 
+            10, labels, marker="o", cmap=cm)
+
+# extract the estimated cluster centres
+cen = kmeans.cluster_centers_
+
+xcen = cen[:, 0]
+ycen = cen[:, 1]
+plt.scatter(xcen, ycen, c="k", marker="d", s=45)
+
+plt.title("Annual Urban population growth(1970 & 2010)")
+plt.xlabel("1970")
+plt.ylabel("2010")
+
+plt.show()
+
+
+
+
 
 
